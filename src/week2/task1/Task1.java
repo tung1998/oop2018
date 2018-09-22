f3b2b8c712b2916c59c9157d1a04661ecf6b46bb
tung1998 <tung16021220@gmail.com>
2018-09-22 21:46:43 +0700

    Add files via upload

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,25 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        if(a>0 || b>0) {
+            while (a != b) {
+                if (a > b) a = a - b;
+                else b = b - a;
+            }
+            return a;
+        }else return 0;
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        if(n<0) return -1;
+        else if (n == 0 || n == 1) return n;
+        else return(fibonacci(n-1) + fibonacci(n-2)) ;
     }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(100, 70));
+        System.out.println(fibonacci(6));
+    };
+
 }
