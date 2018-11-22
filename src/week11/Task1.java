9db580f55fdb14d1f02018606ed94bbe54d54944
tung1998 <tung16021220@gmail.com>
2018-11-22 21:51:25 +0700

    nop bai tuan 9

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,48 @@
 package week11;
-
+    
 public class Task1 {
+    public static <S extends Comparable> void sort(S[] data){
+        int length =data.length;
+        for(int i=0;i<length;i++){
+            for(int j=0;j<length-1;j++){
+                if(data[j].compareTo(data[j+1])>0){
+                    S s= data[j];
+                    data[j]=data[j+1];
+                    data[j+1]=s;
+                }
+            }
+        }
+    }
+    public static <S extends Comparable> void show(S[] data){
+        int length =data.length;
+        System.out.println("sort:");
+        for(int i=0;i<length;i++){
+            System.out.print(data[i]+", ");
+        }
+        System.out.println("\n\n");
+    }
+
+    public static void main(String[] args){
+        Character[] data1= {'w','s','a','i','h'};
+        Integer[] data2= {3,2,647,444,33};
+        Float[] data3={1.1F,44F,63.2F,3.5F};
+        Byte[] data4={-4,44,2,-6};
+        Short[] data5={235,111,3433,99};
+        Long[] data6={44L,22L,32L};
+        Double[] data7={23.3,32.1,53.5,55.2};
+        sort(data1);
+        show(data1);
+        sort(data2);
+        show(data2);
+        sort(data3);
+        show(data3);
+        sort(data4);
+        show(data4);
+        sort(data5);
+        show(data5);
+        sort(data6);
+        show(data6);
+        sort(data7);
+        show(data7);
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,42 @@
 package week11;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Task2 {
+    public static <S extends Comparable> S max(ArrayList<S> n){
+        S max = n.get(0);
+        for(int i=0;i<n.size();i++){
+            if(max.compareTo(n.get(i))<0){
+                max = n.get(i);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args){
+        Character[] data1= {'w','s','a','i','h'};
+        Integer[] data2= {3,2,647,444,33};
+        Float[] data3={1.1F,44F,63.2F,3.5F};
+        Byte[] data4={-4,44,2,-6};
+        Short[] data5={235,111,3433,99};
+        Long[] data6={44L,22L,32L};
+        Double[] data7={23.3,32.1,53.5,55.2};
+        ArrayList<Character> characters = new ArrayList<>(Arrays.asList(data1));
+        System.out.println(max(characters)+"\n");
+        ArrayList<Integer> integers = new ArrayList<>(Arrays.asList(data2));
+        System.out.println(max(integers)+"\n");
+        ArrayList<Byte> bytes = new ArrayList<>(Arrays.asList(data4));
+        System.out.println(max(bytes)+"\n");
+        ArrayList<Short> shorts = new ArrayList<>(Arrays.asList(data5));
+        System.out.println(max(shorts)+"\n");
+        ArrayList<Long> longs = new ArrayList<>(Arrays.asList(data6));
+        System.out.println(max(longs)+"\n");
+        ArrayList<Float> floats = new ArrayList<>(Arrays.asList(data3));
+        System.out.println(max(floats)+"\n");
+        ArrayList<Double> doubles = new ArrayList<>(Arrays.asList(data7));
+        System.out.println(max(doubles)+"\n");
+
+
+    }
 }
